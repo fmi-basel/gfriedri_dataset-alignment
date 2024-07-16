b6ed60d739edc294e4369f6f9c420b14756b38f4
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2024-07-16 11:56:22 +0200

    Add grid_shape parameter.

--- source/s01_parsing_sbem_acquisition/parameter_config.py
+++ source/s01_parsing_sbem_acquisition/parameter_config.py
@@ -5,6 +5,7 @@ class AcquisitionConfig(BaseModel):
     sbem_root_dir: str = ""
     acquisition: str = "run_0"
     tile_grid: str = "g0001"
+    grid_shape: tuple[int, int] = (32, 42)
     thickness: float = 25
     resolution_xy: float = 11
 
