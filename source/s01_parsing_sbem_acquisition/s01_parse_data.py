528a4475287f3edc6295f2f237a4657ccf206159
Ganci <gancarcik.tomas@gmail.com>
2024-08-06 15:34:20 +0200

    Supports parsing SBEMimage project with corrupted or missing metadata

--- source/s01_parsing_sbem_acquisition/s01_parse_data.py
+++ source/s01_parsing_sbem_acquisition/s01_parse_data.py
@@ -1,6 +1,9 @@
 import argparse
 from glob import glob
+from os import scandir
 from os.path import exists, join
+from pathlib import Path
+from typing import Optional
 
 import yaml
 from parameter_config import AcquisitionConfig
@@ -13,6 +16,86 @@ def section_in_range(name: str, start: int, end: int) -> bool:
     s, _ = name.split("_")
     z = int(s[1:])
     return start <= z <= end
+
+
+def get_missing_sections(tile_specs, start_section, end_section) -> set[int]:
+    existing_sec_nums = set()
+    for spec in tile_specs:
+        sec_num = spec["z"]
+        assert isinstance(sec_num, int)
+        existing_sec_nums.add(sec_num)
+    all_sec_nums = set(range(start_section, end_section + 1))
+    return all_sec_nums - existing_sec_nums
+
+
+def get_raw_tile_prefix(directory: Path) -> Optional[str]:
+    """
+    Get the prefix of raw SBEMimage project .tif files without listing all files.
+
+    Example:
+        raw_filename: directory/20230523_RoLi_IV_130558_run2_g0001_t0902_s00299.tif
+        returns: 20230523_RoLi_IV_130558_run2_
+
+    Parameters:
+    directory (str): parent directory to the directories for searching the .tif file.
+
+    Returns:
+    str: Path to the first .tif file, or None if no .tif files are found.
+    """
+    for entry in scandir(directory):
+        if entry.is_file() and entry.name.endswith(".tif"):
+            p = entry.name
+            assert (
+                p[-22] == "g"
+            ), 'Error extracting tile filename (expected suffix pattern form: "g0001_t0902_s00296.tif")'
+            assert (
+                p[-16] == "t"
+            ), 'Error extracting tile filename (expected suffix pattern form: "g0001_t0902_s00296.tif")'
+            assert (
+                p[-10] == "s"
+            ), 'Error extracting tile filename (expected suffix pattern form: "g0001_t0902_s00296.tif")'
+            return entry.name[:-22]
+
+
+def get_missing_tile_specs(
+    missing_section_nums: set[int],
+    sbem_root_dir: str,
+    tile_grid: str,
+    tile_height: int,
+    tile_width: int,
+    overlap: int,
+) -> list[dict]:
+
+    if not missing_section_nums:
+        return []
+
+    dir_tiles = Path(sbem_root_dir) / "tiles" / tile_grid
+    tile_folders = [Path(d) for d in dir_tiles.glob("*") if d.is_dir()]
+    prefix = get_raw_tile_prefix(tile_folders[0])
+    new_tile_specs = []
+
+    for num in missing_section_nums:
+        paths_tiles = []
+        for tf in tile_folders:
+            tile_path = tf / f"{prefix}{tile_grid}_{tf.name}_s{str(num).zfill(5)}.tif"
+            if tile_path.is_file():
+                paths_tiles.append(tile_path)
+
+        for tp in paths_tiles:
+            tile_spec = {
+                "tile_id": int(tp.name[-15:-11]),
+                "grid_num": int(tp.name[-21:-17]),
+                "tile_file": str(tp),
+                "x": 0,
+                "y": 0,
+                "z": int(tp.name[-9:-4]),
+                "overlap": overlap,
+                "tile_height": tile_height,
+                "tile_width": tile_width,
+            }
+            new_tile_specs.append(tile_spec)
+
+    return new_tile_specs
 
 
 def parse_data(
@@ -32,6 +115,19 @@ def parse_data(
 
     tile_specs = get_tile_metadata(
         sbem_root_dir, metadata_files, tile_grid_num, resolution_xy
+    )
+
+    missing_sec_nums = get_missing_sections(tile_specs, start_section, end_section)
+    first_spec = tile_specs[0]
+    tile_specs.extend(
+        get_missing_tile_specs(
+            missing_sec_nums,
+            sbem_root_dir,
+            tile_grid,
+            first_spec["tile_height"],
+            first_spec["tile_width"],
+            first_spec["overlap"],
+        )
     )
 
     sections = {}
@@ -97,7 +193,7 @@ def main(
     end_section: int = 10,
     chunk_size: int = 10,
 ):
-    section_yaml_files = parse_data(
+    parse_data(
         output_dir=output_dir,
         sbem_root_dir=acquisition_conf.sbem_root_dir,
         acquisition=acquisition_conf.acquisition,
